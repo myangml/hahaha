2ff1e936e4cab89de71fe8841fa34518dbc54256
chenyangyang <cyy04202021@163.com>
2021-11-05 20:14:02 +0800

    第一次提交

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
+       <router-link to="/reg">注册</router-link>
     </div>
     <router-view/>
   </div>
---
+++ src/views/Reg.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="reg">
+    <h1>This is an about page</h1>
+    <HelloWorld :msg="msg" />
+  </div>
+</template>
+<script>
+import HelloWorld from "@/components/HelloWorld.vue";
+export default {
+  name: "Reg",
+  data() {
+    return {
+      msg: "我是about传来的值",
+    };
+  },
+  components: {
+    HelloWorld,
+  },
+};
+</script>
